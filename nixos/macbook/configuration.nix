5677ea741005dd7e7cc0b1a9b0a1306a24198872
Marco A. Santana <marco.santana@gmail.com>
2026-03-09 12:50:22 -0600

    macbook: explicitly disable grub to fix systemd-boot installation on hardware

--- nixos/macbook/configuration.nix
+++ nixos/macbook/configuration.nix
@@ -8,6 +8,7 @@
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot";
 
